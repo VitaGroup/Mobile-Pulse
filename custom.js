329c788ffbbc8d23bac58d715f6f3275bb2e4628
Tom Bawden <tommyb@interwebular.com.au>
2013-07-02 11:37:41 +1000

    Add a line of JS which wraps legend tags in a h2, as they are easier to style that way.

--- custom.js
+++ custom.js
@@ -1,17 +1,20 @@
 
 // Left menu stuff
-var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-body = document.body;
- 
-showLeftPush.onclick = function() {
-  classie.toggle( this, 'active' );
-  classie.toggle( body, 'cbp-spmenu-push-toright' );
-  classie.toggle( menuLeft, 'cbp-spmenu-open' );
-  disableOther( 'showLeftPush' );
-};
+if ($('form').hasClass('mobile-home'))
+{
+  var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+  body = document.body;
+   
+  showLeftPush.onclick = function() {
+    classie.toggle( this, 'active' );
+    classie.toggle( body, 'cbp-spmenu-push-toright' );
+    classie.toggle( menuLeft, 'cbp-spmenu-open' );
+  };
+}
 
 // Global News Widget Color Matching
-if ($("div").is("#Widget944"))  {
+if ($("div").is("#Widget944"))  
+{
   var NBNews = $("a[href*='/News/Next-Byte']");
   var NBNEWS = $("a[href*='/NEWS/Next-Byte']");;
   var TelcoNews = $("a[href*='/News/Telecoms']");
@@ -21,18 +24,28 @@ if ($("div").is("#Widget944"))  {
   var VGNews = $("a[href*='/News/Vita-Group']");
   var VGNEWS = $("a[href*='/NEWS/Vita-Group']");
 
-  NBNews.parent().parent().addClass('nb-article');
-  NBNews.parent().parent().addClass('nb-article');
-  NBNEWS.parent().parent().addClass('nb-article');
-  NBNEWS.parent().parent().addClass('nb-article');
-  TelcoNews.parent().parent().addClass('ts-article');
-  TelcoNews.parent().parent().addClass('ts-article');
-  TelcoNEWS.parent().parent().addClass('ts-article');
-  TelcoNEWS.parent().parent().addClass('ts-article');
-  BusNews.parent().parent().addClass('bus-article');
-  BusNews.parent().parent().addClass('bus-article');
-  BusNEWS.parent().parent().addClass('bus-article');
-  BusNEWS.parent().parent().addClass('bus-article');
-  VGNews.parent().parent().addClass('bus-article');
-  VGNEWS.parent().parent().addClass('bus-article');
+  NBNews.parent().parent().parent().addClass('nb-article');
+  NBNEWS.parent().parent().parent().addClass('nb-article');
+  TelcoNews.parent().parent().parent().addClass('ts-article');
+  TelcoNEWS.parent().parent().parent().addClass('ts-article');
+  BusNews.parent().parent().parent().addClass('bus-article');
+  BusNEWS.parent().parent().parent().addClass('bus-article');
+  VGNews.parent().parent().parent().addClass('vg-article');
+  VGNEWS.parent().parent().parent().addClass('vg-article');
+
+  // The links in the 'WidgetPageLinks' make the above function apply vg-article to the parent div, which breaks things. 
+  // Look at refactoring the function
+  $("#Widget944").removeClass('vg-article');
+  $(".column").removeClass('vg-article');
+}
+
+$("#WidgetPageLink944").appendTo($('.container'));
+
+if ($('form').hasClass('vitatube')) {
+  $('.mainItem dl dt br, .subItem dl dt br').remove();
+
+}
+
+if ($('form').hasClass('mobile-forms-view')) {
+  $('legend').wrap('<h2>');
 }
